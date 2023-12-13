107206cf4660fb3167f6afa8fd7f35cc1d2c22e2
KhinMarWin22 <khinmarw658@gmail.com>
2023-12-13 21:17:02 +0630

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    
     extend: {
       fontFamily: {
-        sans: ['"DM Sans"', 'sans-serif'],
-        serif: ['serif'],
+        sans: ["Arial", "sans-serif"],
+        serif: ["Georgia", "serif"],
+        mono :['Monaco','monospace'],
+        DM :['DM Sans', 'sans-serif'],
+        Montserrat :['Montserrat', 'sans-serif'],
+        Rethink :['Rethink Sans', 'sans-serif'],
+        Roboto :['Roboto', "sans-serif"]
       },
-     
     },
   },
   plugins: [],
-}
-
+};
