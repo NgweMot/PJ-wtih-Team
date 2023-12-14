43b10e7e36231cf5a767fbdd14883c19b67d278a
KhinMarWin22 <khinmarw658@gmail.com>
2023-12-14 23:39:11 +0630

    before

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      'sm': {'max': '721px'},
-      // => @media (max-width: 721px) { ... }
-
-      'md':{'max': '992px'},
-      // => @media (max-width: 992px) { ... }
-
-      'lg': '1020px',
-      // => @media (min-width: 1020px) { ... }
-
-      'xl': {'max': '1535px'},
-      // => @media (max-width: 1535px) { ... }
-
-      '2xl': {'max': '1920px'},
-      // => @media (max-width: 1920px) { ... }
-    },
+  
     fontFamily: {
       sans: ["Arial", "sans-serif"],
       serif: ["Georgia", "serif"],
